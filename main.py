f3f9c7537bb99465c5b67a08ad47fc66ad416782
ptka4ev <ptka4ev@mail.ru>
2022-04-11 21:42:49 +0300

    edit menu

--- main.py
+++ main.py
@@ -9,19 +9,29 @@ buttons_batch = pyglet.graphics.Batch()
 pet = Dog('Korgy')
 buttons_img = ['img/sprites/menu/status.png', 'img/sprites/menu/food.png',
                'img/sprites/menu/health.png', 'img/sprites/menu/enjoy.png',
-               'img/sprites/menu/money.png']
+               'img/sprites/menu/money.png', 'img/sprites/menu/selected.png']
 buttons = [pyglet.image.load(img) for img in buttons_img]
+
 status_sprite = pyglet.sprite.Sprite(buttons[0], x=8, y=250, batch=buttons_batch)
 food_sprite = pyglet.sprite.Sprite(buttons[1], x=68, y=250, batch=buttons_batch)
 health_sprite = pyglet.sprite.Sprite(buttons[2], x=128, y=250, batch=buttons_batch)
 enjoy_sprite = pyglet.sprite.Sprite(buttons[3], x=188, y=250, batch=buttons_batch)
 money_sprite = pyglet.sprite.Sprite(buttons[4], x=248, y=250, batch=buttons_batch)
+selected_sprite = pyglet.sprite.Sprite(buttons[5], x=-248, y=-250, batch=buttons_batch)
+
+shape = pyglet.shapes.Rectangle(-300, -300, 282, 200, color=(0, 0, 200))
+shape.opacity = 150
 
 
 # pyglet.font.add_file('fonts/pixeltiny.ttf')
 # pixel_font = pyglet.font.load('PixelTiny')
 # label_name = pyglet.text.Label(pet.name, x=window.width // 2, y=270, anchor_x='center',
 #                                font_name='PixelTiny', font_size=70)
+def on_hint(self, x, y):
+    if self.x <= x <= self.x + self.width and self.y <= y <= self.y + self.height:
+        return True
+    else:
+        return False
 
 
 # @window.event
@@ -29,11 +39,36 @@ money_sprite = pyglet.sprite.Sprite(buttons[4], x=248, y=250, batch=buttons_batc
 #     if symbol == key.LEFT:
 #         pet_sprite.x -= 5
 def on_mouse_press(x, y, button, modifiers):
-    if button == pyglet.window.mouse.LEFT:
-        print(x, y)
+    if on_hint(status_sprite, x, y):
+        selected_sprite.x = status_sprite.x
+        selected_sprite.y = status_sprite.y
+        shape.x = 8
+        shape.y = 45
+    elif on_hint(food_sprite, x, y):
+        selected_sprite.x = food_sprite.x
+        selected_sprite.y = food_sprite.y
+    elif on_hint(health_sprite, x, y):
+        selected_sprite.x = health_sprite.x
+        selected_sprite.y = health_sprite.y
+    elif on_hint(enjoy_sprite, x, y):
+        selected_sprite.x = enjoy_sprite.x
+        selected_sprite.y = enjoy_sprite.y
+    elif on_hint(money_sprite, x, y):
+        selected_sprite.x = money_sprite.x
+        selected_sprite.y = money_sprite.y
 
 
-window.push_handlers(on_mouse_press)
+def on_mouse_release(x, y, button, modifiers):
+    selected_sprite.x = -300
+
+
+# def draw_status():
+#     shape = pyglet.shapes.Rectangle(8, 45, 282, 200, color=(0, 0, 200))
+#     shape.opacity = 150
+#     shape.draw()
+
+
+window.push_handlers(on_mouse_press, on_mouse_release)
 
 
 @window.event
@@ -42,6 +77,7 @@ def on_draw():
     background.blit(0, 0)
     pet.sprite.draw()
     buttons_batch.draw()
+    shape.draw()
 
 
 if __name__ == '__main__':
