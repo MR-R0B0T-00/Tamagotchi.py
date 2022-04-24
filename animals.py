e995e3d9ad128c2e709dfeb6b5df6ba500367a59
ptka4ev <ptka4ev@mail.ru>
2022-04-24 19:06:11 +0300

    refactor animals.py

--- animals.py
+++ animals.py
@@ -9,64 +9,61 @@ class Animal(pyglet.sprite.Sprite):
         self.age = 0
         self.health = 100
         self.hungry = 30
-        self.sleep = 25
+        self.learning = 10
+        self.happy = 50
+        self.toilet = 70
+        self.money = 0
         self.right_move = True
         self.left_move = False
 
     def status(self):
         pass
 
-    def eat(self):
-        if self.hungry > 0:
-            self.hungry -= 10
-            if self.hungry < 0:
-                self.hungry = 0
-        print('Голод --')
-
-    def funny(self):
-        if self.fun < 100:
-            self.fun += 10
-            if self.fun > 100:
-                self.fun = 100
-        print('Веселье ++')
-
-    def birthday(self):
-        self.age += 1
-        print('Возраст ++')
-
-    def to_heal(self):
-        if self.health < 100:
-            self.health += 10
-            if self.health > 100:
-                self.health = 100
-        print('Здоровье ++')
-
-    def to_sleep(self):
-        if self.sleep < 100:
-            self.sleep += 10
-            if self.sleep > 100:
-                self.sleep = 100
-        print('Сон ++')
-
-    def not_eat(self):
-        self.hungry += 10
-        if self.hungry > 100:
-            self.hungry = 100
-        print('Голод ++')
-
-    def not_sleep(self):
-        self.sleep -= 10
-        if self.sleep < 0:
-            self.sleep = 0
-        print('Сон --')
+    # def eat(self):
+    #     if self.hungry > 0:
+    #         self.hungry -= 10
+    #         if self.hungry < 0:
+    #             self.hungry = 0
+    #     print('Голод --')
+    #
+    # def funny(self):
+    #     if self.fun < 100:
+    #         self.fun += 10
+    #         if self.fun > 100:
+    #             self.fun = 100
+    #     print('Веселье ++')
+    #
+    # def birthday(self):
+    #     self.age += 1
+    #     print('Возраст ++')
+    #
+    # def to_heal(self):
+    #     if self.health < 100:
+    #         self.health += 10
+    #         if self.health > 100:
+    #             self.health = 100
+    #     print('Здоровье ++')
+    #
+    # def to_sleep(self):
+    #     if self.sleep < 100:
+    #         self.sleep += 10
+    #         if self.sleep > 100:
+    #             self.sleep = 100
+    #     print('Сон ++')
+    #
+    # def not_eat(self):
+    #     self.hungry += 10
+    #     if self.hungry > 100:
+    #         self.hungry = 100
+    #     print('Голод ++')
 
 
 class Dog(Animal):
     def __init__(self, name, *args, **kwargs):
-        super().__init__(name, load_resources.dog_img[0], *args, **kwargs)
-        self.images = load_resources.dog_img
-        self.animation = pyglet.image.Animation.from_image_sequence(self.images, duration=1 / 3, loop=True)
-        self.sprite = pyglet.sprite.Sprite(self.animation, x=150, y=100)
+        super().__init__(name, *args, **kwargs)
+        self.animation = pyglet.image.Animation.from_image_sequence(load_resources.dog_img, duration=1 / 3, loop=True)
+        self.image = self.animation
+        self.x, self.y = 150, 100
 
     def update(self, dt):  # dt - Это интервал, без него функция не работает
         super(Dog, self).update(dt)
